bdd6ca124f48dd032d66bcfd798a593778e1b1e4
linyin <lujiashuai.777@bytedance.com>
2022-04-23 16:16:47 +0800

    修改

--- myapp_pc/src/main.js
+++ myapp_pc/src/main.js
@@ -3,8 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import settings from './settings';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$settings = settings;
+
+// element-ui配置
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 调用插件
+Vue.use(ElementUI);
+
+// 导入css初始化样式
+import "../static/css/reset.css";
+
+import axios from "axios"
+axios.defaults.withCredentials = false; // false表示阻止ajax附带cookie
+Vue.prototype.$axios = axios; // 把对象挂载vue中
+
 
 /* eslint-disable no-new */
 new Vue({
--- myapp_pc/src/router/index.js
+++ myapp_pc/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
-
+import Home from "../components/Home"
+Vue.use(Router);
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: "Home",
+      component: Home,
+    },{
+      path: '/home',
+      name: "Home",
+      component: Home,
+    },
   ]
 })
+
