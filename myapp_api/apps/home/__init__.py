23578d08a9398a885fdc52ffcbd4c28892cffa8c
linyin <lujiashuai.777@bytedance.com>
2022-04-24 00:26:32 +0800

    新增simpleui后台管理样式

---
+++ myapp_api/apps/home/__init__.py
@@ -0,0 +1,1 @@
+defualt_app_config = 'home.apps.HomeConfig'
--- myapp_api/urls.py
+++ myapp_api/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.urls import re_path
 from django.conf import settings
 from django.views.static import serve
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'media/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r'^', include('home.urls')),
 
 ]
